7f665b4024660092ff095fbcc7c80379d930ed9b
Hgoatly <hgoatly@hotmail.com>
2020-07-29 10:18:15 +0000

    fixed bugs regarding empty divs

--- Adding the map element/assets/js/github-information.js
+++ Adding the map element/assets/js/github-information.js
@@ -32,7 +32,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -63,3 +64,5 @@ function fetchGitHubInformation(event) {
                 }
             });
 }
+
+$(document).ready(fetchGitHubInformation);
